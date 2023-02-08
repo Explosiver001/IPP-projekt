2c8496c003d9f21f41ec6065db2f9c3987ff7e95
Explosiver001 <misel-olsany@seznam.cz>
2023-02-08 21:26:34 +0100

    getopts

--- analyzator_kodu_php/parse.php
+++ analyzator_kodu_php/parse.php
@@ -2,6 +2,15 @@
 
 include 'parse_libs/scanner.php';
 
+$short_options = "h";
+$long_options = array("help");
+
+$options = getopt($short_options, $long_options);
+
+if(array_key_exists("h", $options)){
+    echo "help\n";
+    return;
+}
 
 
 read_lines($input_file);
